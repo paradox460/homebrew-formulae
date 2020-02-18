4a007f6385133bd0fafc88da1332fdeb3542ba59
Jeff Sandberg <paradox460@gmail.com>
2020-02-18 14:02:23 -0700

    Bump Github slack spammer version

--- github_slack_spammer.rb
+++ github_slack_spammer.rb
@@ -1,9 +1,9 @@
 class GithubSlackSpammer < Formula
   desc "A utility for taking open PRs that need approvals, and spamming them into a slack channel."
   homepage "https://github.com/paradox460/github_slack_spammer"
-  version "0.1.0"
+  version "0.1.1"
   url "https://github.com/paradox460/github_slack_spammer/releases/download/v#{version}/github_slack_spammer"
-  sha256 "d361084facce1fc79bee3c27589e260e38e09ab23e435b1be190f4d928c73684"
+  sha256 "12d382cdfcc86ddd5d603ba64a18b1b459830188c995644671feb4065ec9113a"
 
   def install
     bin.install "github_slack_spammer"
