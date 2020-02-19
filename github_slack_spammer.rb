4758839d699633f0d92d8938ee0c64867826b9cb
Jeff Sandberg <paradox460@gmail.com>
2020-02-19 11:09:24 -0700

    GHSS v0.1.4

--- github_slack_spammer.rb
+++ github_slack_spammer.rb
@@ -1,9 +1,9 @@
 class GithubSlackSpammer < Formula
   desc "A utility for taking open PRs that need approvals, and spamming them into a slack channel."
   homepage "https://github.com/paradox460/github_slack_spammer"
-  version "0.1.3"
+  version "0.1.4"
   url "https://github.com/paradox460/github_slack_spammer/releases/download/v#{version}/github_slack_spammer"
-  sha256 "8ea0de3cc6501242eca905f356838c53310d6fb577b61dbb188fef63a1ad3290"
+  sha256 "a2eaa415306eafd154b194f51a5b20570ca763d3ab0cd8ac6ba73eb5f5f480b2"
 
   def install
     bin.install "github_slack_spammer"
